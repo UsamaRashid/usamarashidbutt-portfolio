1a4bafa116221cc9b59cfb5af20f7ef940351fa6
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-09-05 23:24:56 +0500

    WIP: setup grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,26 @@
 export default function Home() {
   return (
-    // <div className='grid grid-cols-2 grid-rows-2 h-full'>
-    //   <div className='col-span-1'>
-    //     <div className=' col-span-2 rounded-lg bg-purple-400 text-white'>
-    //       Top Card
-    //     </div>
-    //   </div>
-
-    //   <div className='col-span-1'>
-    //     <div className=' row-span-1 rounded-lg bg-indigo-400'>Card 5</div>
-    //     <div className='rounded-lg bg-red-400'>Card 6</div>
-    //   </div>
-    // </div>
     <div>
-      <div className='grid grid-cols-12 grid-rows-2 gap-4 h-screen text-red-700'>
-        <div className='col-start-1 col-span-12 rounded-3xl text-center bg-black items-center'>
+      <div className='grid grid-cols-12 grid-rows-2 gap-4 h-screen text-white '>
+        <div className='text-5xl col-start-1 col-span-12 rounded-[4.5rem] text-center flex justify-center items-center'>
           Usama Rashid Butt
         </div>
-        <div className='col-span-6 row-span-2 bg-indigo-800 rounded-3xl text-center'>
+        <div className='col-span-6 row-span-2 bg-violet-800 rounded-[4.5rem] text-center'>
           012
         </div>
-        {/* <div className='col-span-6 row-span-1'> */}
-        <div className='row-span-1 col-span-3 bg-slate-100 rounded-3xl text-center'>
-          222
-        </div>
-        <div className='row-span-1 col-span-3 bg-gray-600 rounded-3xl text-center'>
-          333
-          {/* </div> */}
-        </div>
-        <div className='col-span-6 row-span-1 bg-amber-200 rounded-3xl text-center'>
-          444
+        <div className='col-span-6 grid grid-cols-6 row-span-2 '>
+          <div className='col-span-3 row-span-1 bg-slate-100 rounded-[4.5rem] text-center m-1'>
+            222
+          </div>
+          <div className='col-span-3 row-span-1 bg-gray-900 rounded-[4.5rem] text-center m-1'>
+            333
+          </div>
+          <div className='col-span-6 row-span-1 bg-yellow-200 rounded-[4.5rem] text-center m-1 text-black flex justify-center items-end'>
+            444
+          </div>
         </div>
       </div>
-      <div>Gello</div>
+      <div>GELLLO</div>
     </div>
   );
 }
