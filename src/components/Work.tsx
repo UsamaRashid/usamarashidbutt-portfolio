260562af1ee704ede8a8d70e412f17f56fa41e98
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-09-13 17:00:32 +0500

    [Fix]: Improved Overview textsizes

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -6,7 +6,7 @@ const WorkComponent = () => {
     // h-[32vh] md:h-[40vh] lg:h-[40vh]
     <div className='relative flex flex-col gap-4  rounded-[2.5rem] p-20 px-14 bg-[#1b1b1f] text-[#a5a1ff]'>
       <div className='head text-3xl font-semibold'>Overview.</div>
-      <div className='content sm:text-3xl md:text-4xl '>
+      <div className='content sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl '>
         <br />
         {`With expertise in Blockchain, and
         Fullstack development, I bring a blend of creativity and technical prowess to craft
