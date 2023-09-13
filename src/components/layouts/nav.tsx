7f028cc269d2c5b6612f77b597493b31c3ae7d7f
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-09-13 16:02:27 +0500

    [Fix]: Improved the typewriter font

--- src/components/layouts/nav.tsx
+++ src/components/layouts/nav.tsx
@@ -4,7 +4,11 @@ import { Logo } from "@/icons";
 import { useScroll, useTransform, motion } from "framer-motion";
 import SignatureTypewriter from "../SignatureTypewriter";
 import Typewriter from "typewriter-effect";
+import localFont from "next/font/local";
 
+const Gluten = localFont({
+  src: "../../../public/fonts/Foldit-ExtraBold.ttf",
+});
 const Nav = () => {
   const { scrollYProgress } = useScroll();
   const opacity = useTransform(scrollYProgress, [0, 1], [1, -3]);
@@ -19,7 +23,30 @@ const Nav = () => {
     >
       <div className='m-auto w-fit flex flex-col items-center justify-center'>
         {/* <Logo /> */}
-        <div className='text-2xl sm:text-6xl '>Usama Rashid Butt</div>
+        <div className={Gluten.className}>
+          <div className='text-2xl sm:text-8xl'>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  // .typeString("Hello World!")
+                  .callFunction(() => {
+                    console.log("String typed out!");
+                  })
+                  .pauseFor(40)
+                  .deleteAll()
+                  .callFunction(() => {
+                    console.log("All strings were deleted");
+                  })
+                  .start();
+              }}
+              options={{
+                strings: ["Usama Rashid Butt", "Software Engineer"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
         {/* <div className='text-4xl '>Software Engineer</div> */}
         <div className='sm:text-xl'>
           {/* <Typewriter
