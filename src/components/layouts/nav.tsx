6cc1212255248064d908eb153f9fde778e2b346e
Usama Rashid <usamarashid.work@gmail.com>
2023-10-19 18:14:02 +0500

    fix: font fixed

--- src/components/layouts/nav.tsx
+++ src/components/layouts/nav.tsx
@@ -6,12 +6,12 @@ import SignatureTypewriter from "../SignatureTypewriter";
 import Typewriter from "typewriter-effect";
 import localFont from "next/font/local";
 
-
 const Gluten = localFont({
-  src: "../../../public/fonts/PixelifySans-SemiBold.ttf",
-  // Foldit-ExtraBold.ttf",
+  src: "../../../public/fonts/winter-song-font/WinterSong-owRGB.ttf",
+  // /conquest-font/Conquest-8MxyM.ttf",
+  // /alex-brush/AlexBrush-Regular.ttf",
+  // Gluten-SemiBold.ttf",
 });
-
 
 const Nav = () => {
   const { scrollYProgress } = useScroll();
@@ -23,29 +23,36 @@ const Nav = () => {
         opacity,
         scale,
       }}
-      className='h-[32vh] md:h-[40vh] lg:h-[40vh] grid'
+      className="h-[32vh] md:h-[40vh] lg:h-[40vh] grid"
     >
-      <div className='m-auto w-fit flex flex-col items-center justify-center'>
+      <div className="m-auto w-fit flex flex-col items-center justify-center">
         {/* <Logo /> */}
-        <div className={Gluten.className}>
-          <div className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl'>
+        <div
+          // className="text-5xl"
+          className={Gluten.className}
+        >
+          <div className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
             <Typewriter
               onInit={(typewriter) => {
-                typewriter
-                  .pauseFor(80)
-                  .deleteAll()
-                  .start();
+                typewriter.pauseFor(80).deleteAll().start();
               }}
               options={{
-                strings: ["Usama Rashid Butt", "Software Engineer","Blockchain Enthusiast"],
+                strings: [
+                  "Usama Rashid Butt",
+                  "Software Engineer",
+                  // "Blockchain Enthusiast",
+                  "Usama Rashid Butt",
+                ],
                 autoStart: true,
                 loop: true,
+                // cursor:
+                //  "",
+                // ">",
               }}
             />
           </div>
         </div>
-        <div className='sm:text-xl'>
-        </div>
+        <div className="sm:text-xl"></div>
       </div>
     </motion.nav>
   );
